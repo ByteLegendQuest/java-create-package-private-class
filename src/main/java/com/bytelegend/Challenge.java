1f49ac6e9d1b6ae53c530b26a28119e97a51d26b
初始状态0 <chushi0@users.noreply.github.com>
2022-07-07 12:12:38 +0800

    answer

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,6 @@
 package com.bytelegend;
 
-import java.lang.reflect.Constructor;
+import com.github.blindpirate.extensions.Helper;
 
 public class Challenge {
     public static void main(String[] args) {
@@ -10,19 +10,16 @@ public class Challenge {
     /**
      * Because
      * [`CaptureSystemOutputExtension`](https://github.com/blindpirate/junit5-capture-system-output-extension/blob/4ee3aa0a0d9b2610b482e4571ecc33828c60248a/src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtension.java#L44)
-     * is package-private, you can not create it directly. Please come up with a way to create a
+     * is package-private, you can not create it directly. Please come up with a way
+     * to create a
      * `CaptureSystemOutputExtension` instance.
      *
-     * <p>Hint: You can create some other classes and methods - you don't have to do it in the same
+     * <p>
+     * Hint: You can create some other classes and methods - you don't have to do it
+     * in the same
      * class. Good luck!
      */
     public static Object createCaptureSystemOutputExtension() {
-        try {
-            Constructor<?> constructor = Class.forName("com.github.blindpirate.extensions.CaptureSystemOutputExtension").getDeclearedConstructors()[0];
-            constructor.setAccessible(true);
-            return constructor.newInstance();
-        } catch (Exception e) {
-            return null;
-        }
+        return Helper.newObject();
     }
 }
---
+++ src/main/java/com/github/blindpirate/extensions/Helper.java
@@ -0,0 +1,7 @@
+package com.github.blindpirate.extensions;
+
+public class Helper {
+    public static Object newObject() {
+        return new CaptureSystemOutputExtension();
+    }
+}
