0face8df2f0ad9a6a21f8c8f45a3532cf9a9ecc0
pipiloveslife <Pain_Zzz@163.com>
2022-10-17 16:06:59 +0800

    Create CaptureSystemOutputExtensionBuilder.java

---
+++ src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtensionBuilder.java
@@ -0,0 +1,10 @@
+package com.github.blindpirate.extensions;
+
+/**
+ * @author pipiloveslife
+ */
+public class CaptureSystemOutputExtensionBuilder {
+    public static CaptureSystemOutputExtension build() {
+        return new CaptureSystemOutputExtension();
+    }
+}
